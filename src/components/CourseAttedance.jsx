86d7263d422e377fe0f99ea8abfe8754cd032446
Kathy Dean <kat_dean@me.com>
2018-10-22 19:38:31 -0400

    added template CourseAttendance component

---
+++ src/components/CourseAttedance.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+
+class CourseAttendance extends Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <main>
+          Course Attendance
+
+      </main>
+    )
+  }
+}
+
+export default CourseAttendance;
