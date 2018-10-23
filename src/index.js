276e062876ce68c84701861253486766f8ceb2c2
Pamela Tobon <ptobon@aol.com>
2018-10-22 21:11:43 -0400

    Created TeacherCourse component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CourseAttendance from './components/CourseAttedance'
+import TeachersCourseList from './components/TeachersCourseList'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<CourseAttendance />, document.getElementById('root'));
+ReactDOM.render(<TeachersCourseList />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
