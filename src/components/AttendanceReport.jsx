2c88cbc65f09010911a48e48ec3d721b3d37e5a1
Kathy Dean <kat_dean@me.com>
2018-10-24 18:49:30 -0400

    create report and print components and passed updated data

---
+++ src/components/AttendanceReport.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactToPrint from "react-to-print";
+import AttendanceData from "./AttendanceData"
+
+class AttendanceReport extends React.Component {
+  render() {
+    return (
+      <div>
+        <ReactToPrint
+          trigger={() => <a href="#">Print</a>}
+          content={() => this.componentRef}
+        />
+        <AttendanceData attendanceList={this.props.location.state.detail} ref={el => (this.componentRef = el)} />
+      </div>
+    );
+  }
+}
+
+export default AttendanceReport;
